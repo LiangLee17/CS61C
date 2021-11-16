434cd5246928ded34d80d6c929dd36e551e1a2fb
LiangLee17 <lianglee17@qq.com>
2021-11-16 21:45:16 +0800

    lab01 complete

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,12 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+	if (head == NULL || head->next == NULL) return 0;
+	node *slow = head, *fast = head;
+	while (fast && fast->next) {
+		fast = fast->next->next;
+		slow = slow->next;
+		if (slow == fast) return 1;
+	}
+	return 0;
 }
